bb02d4a4216c6840dfe95b651d835c6a96eff0f1
NicoV00 <6328@holbertonstudents.com>
2024-01-30 08:51:01 -0800

    added task

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""coroutine"""
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> typing.Generator[float, None, None]:
+    """asynchronous coroutine"""
+    for _ in range(0, 10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
