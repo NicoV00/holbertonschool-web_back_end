f64797b019da8938defb8b52b89f559376c249b9
NicoV00 <6328@holbertonstudents.com>
2024-04-26 08:21:30 -0700

    uploaded fixed 6

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue(array, appendString) {
+function appendToEachArrayValue(array, appendString) {
   for (var idx in array) {
     var value = array[idx];
     array[idx] = appendString + value;
@@ -6,3 +6,4 @@ export default function appendToEachArrayValue(array, appendString) {
 
   return array;
 }
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'Hello'));
