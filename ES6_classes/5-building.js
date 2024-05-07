408c0fcac364393c4d2af34c614b8143f004539d
NicoV00 <6328@holbertonstudents.com>
2024-05-07 07:30:22 -0700

    fixed warning message and lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,9 +1,9 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-      throw Error('Class extending Building must override evacuationWarningMessag');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-    this._sqft = sqft
+    this._sqft = sqft;
   }
 
   get sqft() {
