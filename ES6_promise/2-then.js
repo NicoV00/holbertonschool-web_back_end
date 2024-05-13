8f2cb2e52838ed8562a006754d310ad0d6269c1d
NicoV00 <6328@holbertonstudents.com>
2024-05-12 17:30:08 -0700

    fixed lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   const object = {
-    status : 200,
-    body : 'success',
+    status: 200,
+    body: 'success',
   };
   return promise
-    .then(() => {
-      return object;
-    })
-    .catch(() => {
-      return new Error();
-    })
+    .then(() => object)
+    .catch(() => new Error())
     .finally(() => {
-      console.log('Got a response from the API')
+      console.log('Got a response from the API');
     });
 }
