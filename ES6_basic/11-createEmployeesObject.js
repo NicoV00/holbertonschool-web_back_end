f6b4525516e18153064e2371f9c97482e86d3ac0
NicoV00 <6328@holbertonstudents.com>
2024-05-12 18:10:47 -0700

    fixed style

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const EmployeesObjec = {
-    [`${departmentName}`] : employees,
+    [`${departmentName}`]: employees,
   };
   return EmployeesObjec;
 }
