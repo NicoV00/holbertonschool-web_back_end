e8b3b2939aa9f655911bfb6e0eea9d667b0d2cf5
NicoV00 <6328@holbertonstudents.com>
2024-05-22 09:02:58 -0700

    added task 10 wrong py

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """task 10"""
 
+
 def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on name"""
+    uptop = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    return uptop
