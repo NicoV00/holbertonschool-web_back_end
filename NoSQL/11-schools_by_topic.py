fe8296e6fbe160a5e66aff1bfa139752b76ff83f
NicoV00 <6328@holbertonstudents.com>
2024-05-22 08:34:46 -0700

    uploaded task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """task 11"""
 
+
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
-    
+    topschool = mongo_collection.find({"topics" : topic})
+    return topschool
