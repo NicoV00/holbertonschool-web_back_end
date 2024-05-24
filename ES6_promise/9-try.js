e43dae67ceb1e69192ceb3a339a031a40c3948e0
NicoV00 <6328@holbertonstudents.com>
2024-05-24 12:08:26 -0700

    added task 9, functioning

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,3 +1,11 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-}
+  try {
+    const valor = mathFunction();
+      queue.push(valor);
+  } catch (err) {
+    queue.push(err.toString());
+  }
+  queue.push('Guardrail was processed')
+  return queue;
+} 
