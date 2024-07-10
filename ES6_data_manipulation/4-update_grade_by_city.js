0505b378930353f899c957cbbe1cd3ae4ebb17db
NicoV00 <6328@holbertonstudents.com>
2024-07-10 09:47:51 -0700

    added task

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,10 @@
-export default function updateStudentGradeByCity() {
-
+export default function updateStudentGradeByCity(Students, city, newGrades) {
+  return Students.filter((student) => student.location === city)
+  .map((student) => {
+      const notaS =  newGrades.find((nota) => nota.studentId === student.id);
+      return {
+        ...student,
+        nota: notaS ? notaS.nota : 'N/A'
+      };
+    });
 }
