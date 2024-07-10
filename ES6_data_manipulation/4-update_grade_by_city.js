20b455f3fc732ef2c1ea6fb927457e52d16ee38c
NicoV00 <6328@holbertonstudents.com>
2024-07-10 10:07:52 -0700

    changed lint and also already predetermined names

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,11 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-  .filter((student) => student.location === city)
-  .map((student) => {
-    const notaFinal = newGrades.find((nota) => nota.studentId === student.id);
-    return {
-      ...student,
-      grade: notaFinal ? notaFinal.grade : 'N/A'
-    };
-  });
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const notaFinal = newGrades.find((nota) => nota.studentId === student.id);
+      return {
+        ...student,
+        grade: notaFinal ? notaFinal.grade : 'N/A'
+      };
+    });
 }
