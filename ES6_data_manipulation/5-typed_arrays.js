2d6ab412e0d38408742fa458e77b396e2ef31889
NicoV00 <6328@holbertonstudents.com>
2024-07-10 10:19:43 -0700

    uploaded task

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,11 @@
-export default function createInt8TypedArray() {
+export default function createInt8TypedArray(length, position, value) {
+  const arrayBuffer = new ArrayBuffer(length);
+  const data = new DataView(arrayBuffer);
 
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  data.setInt8(position, value);
+  return data;
 }
