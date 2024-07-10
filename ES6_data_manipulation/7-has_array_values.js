da3ca1ab1a7a1f444e5c93b4b436ef7f258be021
NicoV00 <6328@holbertonstudents.com>
2024-07-10 12:49:09 -0700

    sdasd

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,4 @@
-export default function hasValuesFromArray() {
-
+export default function hasValuesFromArray(set, array) {
+    const matchArr = Array.from(set);
+    return array.every(num => matchArr.includes(num));
 }
