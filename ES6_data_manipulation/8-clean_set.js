bca85e81024faccb1b8bd77c7a0209f1b2b3a8a4
NicoV00 <6328@holbertonstudents.com>
2024-07-10 14:05:34 -0700

    8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (startString === undefined || typeof startString !== 'string') {
+    return '';
+  }
+
   let result = [];
 
   Array.from(set).forEach(string => {
