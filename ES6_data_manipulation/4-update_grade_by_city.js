46b7271ef18c12dc281621e8034779e22e0a30c5
NicoV00 <6328@holbertonstudents.com>
2024-07-10 10:02:12 -0700

    test

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
   .filter((student) => student.location === city)
   .map((student) => {
-      const notaFinal = newGrades.find((nota) => nota.studentId === student.id);
-      return {
-        ...student,
-        nota: notaFinal ? notaFinal.grade : 'N/A'
-      };
-    });
+    const notaFinal = newGrades.find((nota) => nota.studentId === student.id);
+    return {
+      ...student,
+      grade: notaFinal ? notaFinal.grade : 'N/A'
+    };
+  });
 }
